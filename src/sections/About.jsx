287062f85f7cc9d16982ece1c19490e3f94fda70
Vamsi krishna nouluri <92721694+vamsi633@users.noreply.github.com>
2024-09-25 17:15:51 -0700

    Update About.jsx

--- src/sections/About.jsx
+++ src/sections/About.jsx
@@ -23,7 +23,7 @@ const About = () => {
             <img src="assets/grid1.png" alt="grid-1" className="w-full sm:h-[276px] h-fit object-contain" />
 
             <div>
-              <p className="grid-headtext">Hi, I’m Adrian Hajdin</p>
+              <p className="grid-headtext">Hi, I’m Vamsikrishna Nouluri</p>
               <p className="grid-subtext">
                 With 12 years of experience, I have honed my skills in both frontend and backend dev, creating dynamic
                 and responsive websites.
