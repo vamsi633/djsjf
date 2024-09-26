52bb7e9d1516288ee626831afd7791ead9dff61f
Vamsi krishna nouluri <92721694+vamsi633@users.noreply.github.com>
2024-09-25 17:17:52 -0700

    Update Hero.jsx

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -25,12 +25,10 @@ const Hero = () => {
   return (
     <section className="min-h-screen w-full flex flex-col relative" id="home">
       <div className="w-full mx-auto flex flex-col sm:mt-36 mt-20 c-space gap-3">
-        <p className="hero_tag text-gray_gradient">  Hi, I am Vamsi 
-          <span className="waving-hand">👋</span>
-        </p>
         <p className="sm:text-3xl text-xl font-medium text-white text-center font-generalsans">
-          Currently pursuing Masters in Computer Science at Santa Clara University
+          Hi, I am Vamsi <span className="waving-hand">👋</span>
         </p>
+        <p className="hero_tag text-gray_gradient">Building Products & Brands</p>
       </div>
 
       <div className="w-full h-full absolute inset-0">
