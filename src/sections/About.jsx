999df1a144a9ffe0fb2593f6a79cfd3fe5242a73
Vamsi krishna nouluri <92721694+vamsi633@users.noreply.github.com>
2024-09-25 17:27:23 -0700

    Update About.jsx

--- src/sections/About.jsx
+++ src/sections/About.jsx
@@ -94,7 +94,7 @@ const About = () => {
               <p className="grid-subtext text-center">Contact me</p>
               <div className="copy-container" onClick={handleCopy}>
                 <img src={hasCopied ? 'assets/tick.svg' : 'assets/copy.svg'} alt="copy" />
-                <p className="lg:text-2xl md:text-xl font-medium text-gray_gradient text-white">vamsikrishnanouluri@gmail.com</p>
+                <p className="lg:text-xl md:text-xl font-medium text-gray_gradient text-white">vamsikrishnanouluri@gmail.com</p>
               </div>
             </div>
           </div>
